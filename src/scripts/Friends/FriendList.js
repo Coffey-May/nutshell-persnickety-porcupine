0fb2665d7fcfd59b4c83f47e3c38850e053282dc
ETamez9173 <elitamez17@gmail.com>
2020-01-16 14:49:25 -0600

    more friendslist code

--- src/scripts/Friends/FriendList.js
+++ src/scripts/Friends/FriendList.js
@@ -1,34 +1,65 @@
+// 
+// Author: Eli Tamez cohort 37
+// Purpose of Module : This module represents the application state change when new friend
+// relationships are initiatied by 1) adding by doing a friend text search by name and add
+// or 2) by clickAdding a friend from the Chats using the click user name and add feature
+
+
 import { useFriends } from "./FriendProvider.js"
-import Friend from "./User.js"
+// import FriendComponent from "./Friend.js"
 const eventHub = document.querySelector("container");
-const contentTarget = document.querySelector("userForm")
+const contentTarget = document.querySelector("friendsList")
 
-const UserList = () =>   {
+const FriendList = () =>   {
 
 
-    const appStateUsers = useUsers()
+    const appStateFriends = useFriends()
 
     // Add the eventHub lister
     eventHub.addEventListener("activeUserLoggedIn", event => {
         render([])
     })
 
+//  Put code here to filter the friends based on the join table "friends"
+// where the userId = user.id                    and friendName === userEmail
+
+// {
+    // "users": [
+    //   {
+    //     "id": 1,
+    //     "userName": "adi@nss.com",
+    //     "userEmail": "adi@nss.com",
+    //     "userPassword": "1234"
+    //   },
+    //   {
+    //     "id": 2,
+    //     "userName": "cof@nss.com",
+    //     "userEmail": "cof@nss.com",
+    //     "userPassword": "1234"
+    //   }
+    // ],
+    // "friends": [
+    //   {
+    //     "id": 1,
+    //     "friendName": "coffee@nss.com",
+    //     "userId": "2"
+    //   }
 
 
 
 
-    const render = users => {
+    const render = friends => {
         contentTarget.innerHTML = `
         <div>
             <div>
-            <h2> Nutshell Users </h2>
+            <h2> Nutshell Friends </h2>
             </div>
-        <section class="userComponent">     
+        <section class="friendComponent">     
         
-        <div class="user">
+        <div class="friend">
                 ${
-            users.map(userObject => {
-                const userHTML = User(userObject)
+            friends.map(friendObject => {
+                const userHTML = Friend(friendObject)
                 return userHTML
             }).join("")
             }
@@ -39,8 +70,8 @@ const UserList = () =>   {
 
     };
 
-    render(appStateUsers)
+    render(appStateFriends)
 
 }
 
-export default UserList
+export default FriendList
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 import { userFormComponent } from "./Users/UserForm.js";
 
+
+import FriendList from "./Friends/FriendList.js"
+
+
+
 userFormComponent()
+
+
+// getFriends().then(
+    // () =>  FriendList()
+//  )
+
+FriendList()
