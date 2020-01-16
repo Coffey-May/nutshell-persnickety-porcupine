6143c8034cfc8db440b9a9d93a01ec399fc62108
rebeccapatek <rebeccapatek@gmail.com>
2020-01-16 15:10:15 -0600

    Working on userform conditional statemtns

--- src/scripts/Users/UserForm.js
+++ src/scripts/Users/UserForm.js
@@ -1,5 +1,7 @@
-//Coffey html temlate insert.
+import { useUsers, saveUser } from "./UserProvider";
 
+//Coffey html temlate insert.
+//Rebecca made changes and added functionality/eventHub
 const eventHub = document.querySelector('.container');
 const contentTarget = document.querySelector('.userForm');
 
@@ -9,22 +11,56 @@ const contentTarget = document.querySelector('.userForm');
 
 
 export const userFormComponent = () => {
+    eventHub.addEventListener("click", clickEvent => {
+        const allUsersArray = useUsers()
+        const userName = document.querySelector("#userName").value
+        const password1 = document.querySelector("#password1").value
+        const password2 = document.querySelector("#password2").value
+        
+    if (clickEvent.target.id === "saveNewUser"){
+        if (userName != "" && password1 != "") {
+            const foundMatchingUser = allUsersArray.find(user => user.userName === userName)
+            if (foundMatchingUser = undefined) {
+                if (password1 === password2) {
+                const newUser = {
+                    "userName": document.querySelector("#userName").value,
+                    "userPassword": document.querySelector("#password1").value
+                
+                }
+                saveUser(newUser)
+            }
+            else {
+                alert ("passwords do not match")
+            }
+            }
+            else {
+                alert("Account alread exists")
+            }
+        }
+        else {
+            alert("Please fill out all fields")
+        }
+            
+        }
+    }
+    )}
+
 	const render = () => {
 		contentTarget.innerHTML = `
 <h1>WELCOME TO NUTSHELL</h1>
 <div>
     <form action="">
         <label for="">E-MAIL</label>
-        <input type="email">
+        <input type="email" id="userName">
         <label for="">Password</label>
-        <input type="passwaord">
+        <input type="password" id="password1>
         <button>REGISTER</button>
         <label for="">CONFIRM PASSWORD</label>
-        <input type="password">
+        <input type="password" id="password2>
     </form>
-    <button>SAVE NEW ACCOUNT</button>
+    <button id="saveNewUser">SAVE NEW ACCOUNT</button>
 </div>
 `;
 };
 render();
-};
+
