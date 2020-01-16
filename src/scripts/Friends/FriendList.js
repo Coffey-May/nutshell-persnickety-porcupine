b2874a64bd57ca5cfa84a08f664316f332bd384a
ETamez9173 <elitamez17@gmail.com>
2020-01-16 15:32:41 -0600

    added filter idea

--- src/scripts/Friends/FriendList.js
+++ src/scripts/Friends/FriendList.js
@@ -1,24 +1,40 @@
 // 
 // Author: Eli Tamez cohort 37
-// Purpose of Module : This module represents the application state change when new friend
-// relationships are initiatied by 1) adding by doing a friend text search by name and add
+// Purpose of Module : This module represents the application state change when a new friend
+// relationship is initiatied by 1) adding a friend by doing a friend text search by name and
+// chosing to add the friend
 // or 2) by clickAdding a friend from the Chats using the click user name and add feature
+// User are the same as friends so we use Users and filter them based on the friends join 
+// the database.json file
 
 
 import { useFriends } from "./FriendProvider.js"
+import { useUsers } from "../Users/UserProvider.js"
 // import FriendComponent from "./Friend.js"
-const eventHub = document.querySelector("container");
-const contentTarget = document.querySelector("friendsList")
+const eventHub = document.querySelector(".container");
+const contentTarget = document.querySelector(".friendsList")
 
 const FriendList = () =>   {
 
+    // get the join objects to do the filter from the FriendProvider.js
+    const appFriends = useFriends()
 
-    const appStateFriends = useFriends()
+    // get the user aka friends from the UserProvider.js to filter only friends
+    const appStateFriends = useUsers()
+    // const appStateFriends = useFriends()
+
+
+//// TESTING THIS AS THE FILTER
+    // const journalMood = theJournals.filter(theMood => mood === theMood.mood)
+
+   
+    // render(journalMood)
+//// TESTING THIS AS THE FILTER
 
     // Add the eventHub lister
-    eventHub.addEventListener("activeUserLoggedIn", event => {
-        render([])
-    })
+    // eventHub.addEventListener("activeUserLoggedIn", event => {
+    //     render([])
+    // })
 
 //  Put code here to filter the friends based on the join table "friends"
 // where the userId = user.id                    and friendName === userEmail
