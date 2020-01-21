1c1c731e8df72eb1cf697a6a2034f29e2e0b4543
AdiGarmendia <dejima@gmail.com>
2020-01-21 08:58:47 -0600

    chat I hate joo

--- src/scripts/Articles/ArticleProvider.js
+++ src/scripts/Articles/ArticleProvider.js
@@ -41,7 +41,6 @@ export const saveArticle = article => {
  return fetch('http://localhost:8088/articles', {
 =======
    return fetch('http://localhost:8088/articles', {
->>>>>>> master
         method: "POST",
         headers: {
             "Content-Type": "application/json"
