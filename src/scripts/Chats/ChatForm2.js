9af4b6e8e4ca50b399b40b760c561e1d6848ef82
ETamez9173 <elitamez17@gmail.com>
2020-01-21 12:06:09 -0600

    working on adding friend using the userName in chats

---
+++ src/scripts/Chats/ChatForm2.js
@@ -0,0 +1,43 @@
+export const ChatFormComponent = () => {
+
+
+    eventHub.addEventListener("click", clickEvent => {
+  
+      if (clickEvent.target.id === "addBtnChatFriend") {
+  
+        const chats = useChats();
+        const userName = document.querySelector("chatFriendCardName").value;
+        
+  
+        
+  
+          const contentTarget = document.querySelector(".friends");
+          const friendToAddId = foundUser.id
+  
+          /// Populate the friends object  
+          const createNewFriendJoin = {
+  
+            userId: userName,
+            initiatorId: parseInt(activeUserInitiatorId)
+  
+          }
+  
+  
+          saveFriend(createNewFriendJoin).then(
+            () => {
+              /// Important to get the latest friends to do the render here.  Without it there is a lag in application state
+              /// or the friends listed will be one transaction behind
+              const afterSaveFriends = useFriends()
+              const reallyUpdatedFriendAfterSave = afterSaveFriends.filter(FriendRel => parseInt(activeUserInitiatorId) === parseInt(FriendRel.initiatorId))
+              const message = new CustomEvent("newFriendJoinCreated")
+  
+              eventHub.dispatchEvent(message)
+              render(reallyUpdatedFriendAfterSave)
+            }
+          )
+  
+        }
+      }
+    }
+  
+    )
